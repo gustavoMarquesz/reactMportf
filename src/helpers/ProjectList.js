6ad4d930346ec38533361a61cd4d892b13653314
gustavo josé <gustavo99a@hotmail.com>
2022-09-29 09:37:12 -0300

    mais projetos adicionados

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -2,12 +2,14 @@ import Contador from '../assets/contador.jfif'
 import TodoReact from '../assets/example-three.jpeg'
 import GalhoSeco from '../assets/galho-seco.jpeg'
 import TodoVanilla from '../assets/todolist.jfif'
+import Milhao from '../assets/milhao.jpeg'
+import Port from '../assets/portf.jpeg'
 
 export const ProjectList = [
     {
-        name: "Todo-List | HTML, CSS e JS Vanilla",
-        image: TodoVanilla,
-        description: "Como o título sugere, ele foi feito com estas tecnologias. Foi o primeiro projeto todo que fiz. Muito bom para treinar operações de CRUD"
+        name: "Roda a Roda do milhão",
+        image: Milhao,
+        description: "Jogo inspirado nos jogo Roda a Roda do SBT |  Você já começa com 50 reais e, se perder no primeiro turno, você leva pra casa os 50, mas se deseja participar de novo, você inicia com 0."
     },
 
     {
@@ -17,17 +19,30 @@ export const ProjectList = [
     },
 
     {
+        name: "Todo-List | HTML, CSS e JS Vanilla",
+        image: TodoVanilla,
+        description: "Como o título sugere, ele foi feito com estas tecnologias. Foi o primeiro projeto todo que fiz. Muito bom para treinar operações de CRUD"
+    },
+
+
+    {
         name: "Ateliê Galho Seco",
         image: GalhoSeco,
         description: "Visando ajudar meus pais, que começaram um negocio, mais precisamente um ateliê e estudar HTML + CSS, fiz uma página estática para eles. "
     },
 
     {
+        name: "Portfólio | Primeira versão",
+        image: Port,
+        description: "No primeiro contato que tive com HTML, CSS e Js, criei a primeira versão do meu portfólio."
+    },
+
+    {
         name: "DecolaTech Counter",
         image: Contador,
         description: "Desafio proposto no bootCamp da dio Avanade-DecolaTech, onde o contador não conta números negativos e só até 10."
+    },
 
-    }
   
 ]
 
